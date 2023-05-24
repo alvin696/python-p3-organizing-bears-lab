36679ac85df9f666e4d1fe6c8daf9a5924074a66
Alvin kipkirui yaem <alvinyaem4@gmail.com>
2023-05-24 09:43:38 +0300

    Completed assignment

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+    ('Grinch', 2, 'M', 'brown', 'grumpy', 1),
+    ('Melissa', 13, 'F', 'black', 'friendly', 1),
+    ('Mr. Chocolate', 20, 'M', 'brown', 'grumpy', 1),
+    ('Rowdy', 10, 'M', 'brown', 'friendly', 1),
+    ('Sergeant Brown', 5, 'M', 'brown', 'grumpy', 0),
+    ('Tabitha', 6, 'F', 'black', 'friendly', 1),
+    ('Wendy', 6, 'F', 'brown', 'friendly', 1),
+    ('Unnamed', 0, '', '', '', 1);
--- lib/testing/insert_test.py
+++ lib/testing/insert_test.py
@@ -24,6 +24,6 @@ class TestInsert:
 
     def test_has_unnamed_bear(self):
         '''inserts one unnamed bear into bears table.'''
-        result = cursor.execute("SELECT COUNT(*) FROM bears WHERE name IS NULL;")
+        result = cursor.execute("SELECT COUNT(*) FROM bears WHERE name IS 'Unnamed';" )
         assert(result.fetchall()[0][0] == 1)
     
